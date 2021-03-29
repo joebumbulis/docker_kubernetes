d055161b1dc4c5593ed2119b7eaf4cabac011c85
joebumbulis <joebumbulis@gmail.com>
2021-03-29 17:08:00 -0500

    docker compose with redis and node

--- multi_container_visit_app/index.js
+++ multi_container_visit_app/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379,
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+  process.exit(0);
   client.get('visits', (err, visits) => {
-    res.send('Number of visits is ' + visits); 
-    client.set('visits', parseInt(visits) +1);
+    res.send('Number of visits ' + visits);
+    client.set('visits', parseInt(visits) + 1);
   });
 });
 
@@ -17,3 +22,5 @@ app.get('/', (req, res) => {
 app.listen(8081, () => {
   console.log('Listening on port 8081');
 })
+
+
